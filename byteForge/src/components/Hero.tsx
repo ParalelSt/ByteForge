6dac2ed3bc1eef29d043da9c406201762ec2ebc1
AronM <strixparalel@gmail.com>
2025-10-29 19:06:54 +0100

    feat: added section container - container for elements of each section

---
+++ byteForge/src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import SectionContainer from "./SectionContainer";
+
+const Hero = () => {
+  return (
+    <>
+      <SectionContainer className="hero-section">
+        <span className="Test">Test</span>
+      </SectionContainer>
+    </>
+  );
+};
+
+export default Hero;
