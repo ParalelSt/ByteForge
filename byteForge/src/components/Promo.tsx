d2e322d78925f24da89eb56a44063fc7c3cac4ee
AronM <strixparalel@gmail.com>
2025-11-05 18:19:10 +0100

    fix: promo styles - layout

--- byteForge/src/components/Promo.tsx
+++ byteForge/src/components/Promo.tsx
@@ -6,20 +6,16 @@ const Promo = () => {
   return (
     <div className="promo-container">
       <div className="promo-container-top">
-        <div className="promo-container-top-left">
-          <img src="" alt="promo-image" className="promo-image" />
-        </div>
-        <SectionContainer className="promo-container-top-right">
-          <h2 className="promo-title">PROMO SECTION</h2>
-          <p className="promo-text">
-            Lorem ipsum adispiscing amet ofenim erodit. Lorem ipsum adispiscing
-            amet ofenim
-          </p>
-        </SectionContainer>
+        <img src="" alt="promo-image" className="promo-image" />
       </div>
-      <div className="promo-container-bottom">
+      <SectionContainer className="promo-container-bottom">
+        <h2 className="promo-title">PROMO SECTION</h2>
+        <p className="promo-text">
+          Lorem ipsum adispiscing amet ofenim erodit. Lorem ipsum adispiscing
+          amet ofenim
+        </p>
         <ShopNowBtn link="/shop/promo" />
-      </div>
+      </SectionContainer>
     </div>
   );
 };
