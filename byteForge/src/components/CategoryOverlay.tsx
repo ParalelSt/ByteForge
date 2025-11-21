c9839c405f20c47d794b9b767e62986436007e0f
Aron Matoic <128235732+ParalelSt@users.noreply.github.com>
2025-11-21 19:32:41 +0100

    fix: category styles

--- byteForge/src/components/CategoryOverlay.tsx
+++ byteForge/src/components/CategoryOverlay.tsx
@@ -3,6 +3,8 @@ import {
   CATEGORY_LABELS,
   type CategoryKey,
 } from "@/api/categoryData";
+import "@/styles/categoryOverlay.scss";
+import { RiArrowLeftSLine, RiCloseLine } from "react-icons/ri";
 
 interface CategoryOverlayProps {
   category: CategoryKey | null;
@@ -26,27 +28,30 @@ const CategoryOverlay = ({
   return (
     <div className="overlay-container">
       <div className="overlay-top">
-        <button className="overlay-back" onClick={onBack}>
-          &lt; BACK
-        </button>
+        <div className="overlay-btns">
+          <button className="overlay-back-btn" onClick={onBack}>
+            <RiArrowLeftSLine /> BACK
+          </button>
 
-        <h2 className="overlay-title">{CATEGORY_LABELS[category]}</h2>
-
-        <button className="overlay-close" onClick={onClose}>
-          X
-        </button>
-
-        <div className="overlay-list">
-          {CATEGORY_ITEMS[category].map((item: string) => (
-            <button
-              key={item}
-              className="overlay-list-item"
-              onClick={() => onSelectSubCategory(item)}
-            >
-              {item}
-            </button>
-          ))}
+          <button className="overlay-close-btn" onClick={onClose}>
+            <RiCloseLine />
+          </button>
         </div>
+
+        <div className="overlay-title-container">
+          <h2 className="overlay-title">{CATEGORY_LABELS[category]}</h2>
+        </div>
+      </div>
+      <div className="overlay-list">
+        {CATEGORY_ITEMS[category].map((item: string) => (
+          <button
+            key={item}
+            className="overlay-list-item"
+            onClick={() => onSelectSubCategory(item)}
+          >
+            {item}
+          </button>
+        ))}
       </div>
     </div>
   );
