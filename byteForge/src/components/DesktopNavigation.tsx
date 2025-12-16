c1520be73dc3fb4806bf37a388a5334a0a25b509
Aron Matoic <128235732+ParalelSt@users.noreply.github.com>
2025-12-16 17:08:38 +0100

    feat: added drop shadow to nav selection on desktop

--- byteForge/src/components/DesktopNavigation.tsx
+++ byteForge/src/components/DesktopNavigation.tsx
@@ -1,19 +1,23 @@
 import "@/styles/navigation.scss";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const DesktopNavigation = () => {
+  const location = useLocation();
+
+  const isActive = (path: string) => location.pathname === path;
+
   return (
     <nav className="desktop-nav">
-      <Link to={"/shop"}>
+      <Link className={isActive("/shop") ? "active" : ""} to="/shop">
         <span>Shop</span>
       </Link>
-      <Link to={"/about"}>
+      <Link className={isActive("/about") ? "active" : ""} to="/about">
         <span>About</span>
       </Link>
-      <Link to={"/contact"}>
+      <Link className={isActive("/contact") ? "active" : ""} to="/contact">
         <span>Contact</span>
       </Link>
-      <Link to={"/account"}>
+      <Link className={isActive("/account") ? "active" : ""} to="/account">
         <span>Account</span>
       </Link>
     </nav>
